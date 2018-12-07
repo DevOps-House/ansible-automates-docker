24f4d483d384626459a3d5bd05ca2351cc93b7b4
Ompragash Viswanathan <ompragash@localhost.localdomain>
2018-12-07 12:09:14 +0530

    Deploy Containers Bash Script

---
+++ deploy_containers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+printf "\033[1;31mEnter the Image name(Eg: rhel, fedora, ubuntu, etc):\033[0m\n"
+read imagename
+printf "\033[1;31mNumber of containers to be launched:\033[0m\n"
+read int
+
+docker pull $imagename 1> /dev/null 2>&1
+returncode=`echo $?`
+
+if [ $returncode -eq 0 ]; then
+a=$(docker images |grep -i $imagename |awk {'print $3'})
+for i in `seq $int` ; do docker run -it -d $a bash > /dev/null; done
+printf "\033[1;31m$int Launch completed.\033[0m\n"
+else
+printf "\033[1;31mOops!! $imagename is not available in repository.\033[0m\n"
+fi
+
